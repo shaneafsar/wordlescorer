49c3a529166ad2e89010af0bb8c05cbc39d6380c
Shane Afsar <shane.afsar@gmail.com>
2024-03-16 15:59:23 +0000

    fix wordle number parsing logic

--- js/extract/get-wordle-number-from-text.js
+++ js/extract/get-wordle-number-from-text.js
@@ -1,16 +1,29 @@
 /**
- * Get the wordle number using regex from a text string. For example
- * if given a string Wordle 456 5/6, return 456.
+ * Get the wordle number using regex from a text string. This function first checks if the Wordle number includes
+ * a comma (formatted as "1,001"). If no comma is present, it attempts to extract a regular number.
+ * It supports extracting Wordle numbers both with and without comma formatting.
+ * For example, if given a string "Wordle 1,001 5/6", it returns 1001. If given "Wordle 456 5/6", it returns 456.
  * @param {String} text - input text to convert to wordle number
  * @returns {Number}
  */
 function getWordleNumberFromText(text = '') {
-  let wordle = text.match(/wordle\s*#?\s*(\d+)/i);
-  if(wordle === null || wordle.length < 2) {
-      return 0;
+  // First try to match a number with commas
+  let wordleWithComma = text.match(/wordle\s*#?\s*(\d{1,3}(?:,\d{3})+)/i);
+  if(wordleWithComma && wordleWithComma.length >= 2) {
+    // If a comma-inclusive number is found, remove commas and convert to number
+    let numberWithoutCommas = wordleWithComma[1].replace(/,/g, '');
+    return parseInt(numberWithoutCommas);
   }
-  //convert wordle[1] to number
-  return parseInt(wordle?.[1] || '0');
+
+  // If no comma-inclusive number is found, try to match any number
+  let wordleWithoutComma = text.match(/wordle\s*#?\s*(\d+)/i);
+  if(wordleWithoutComma && wordleWithoutComma.length >= 2) {
+    // Convert found number to integer
+    return parseInt(wordleWithoutComma[1]);
+  }
+
+  // Return 0 if no matching number is found
+  return 0;
 }
 
 /**
