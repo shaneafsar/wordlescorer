4b4f03ec72475c8751375e710e28664e78e1aa5d
Shane Afsar <shane.afsar@gmail.com>
2024-03-17 13:24:09 +0000

    fix formatting extraction logic for wordle number to include non-US style

--- js/extract/get-wordle-number-from-text.js
+++ js/extract/get-wordle-number-from-text.js
@@ -1,25 +1,26 @@
 /**
- * Get the wordle number using regex from a text string. This function first checks if the Wordle number includes
- * a comma (formatted as "1,001"). If no comma is present, it attempts to extract a regular number.
- * It supports extracting Wordle numbers both with and without comma formatting.
- * For example, if given a string "Wordle 1,001 5/6", it returns 1001. If given "Wordle 456 5/6", it returns 456.
+ * Get the Wordle number using regex from a text string. This function first checks if the Wordle number includes
+ * a comma or period (formatted as "1,001" or "1.001") as thousands separators. If no comma or period is present,
+ * it attempts to extract a regular number. It supports extracting Wordle numbers both with and without comma or
+ * period formatting. For example, if given a string "Wordle 1,001 5/6" or "Wordle 1.001 5/6", it returns 1001.
+ * If given "Wordle 456 5/6", it returns 456.
  * @param {String} text - input text to convert to wordle number
  * @returns {Number}
  */
 function getWordleNumberFromText(text = '') {
-  // First try to match a number with commas
-  let wordleWithComma = text.match(/wordle\s*#?\s*(\d{1,3}(?:,\d{3})+)/i);
-  if(wordleWithComma && wordleWithComma.length >= 2) {
-    // If a comma-inclusive number is found, remove commas and convert to number
-    let numberWithoutCommas = wordleWithComma[1].replace(/,/g, '');
-    return parseInt(numberWithoutCommas);
+  // Try to match a number with commas or periods as thousands separators
+  let wordleFormatted = text.match(/wordle\s*#?\s*(\d{1,3}(?:[,.]\d{3})+)/i);
+  if(wordleFormatted && wordleFormatted.length >= 2) {
+    // If a formatted number is found, remove commas or periods and convert to number
+    let numberWithoutSeparators = wordleFormatted[1].replace(/[,.]/g, '');
+    return parseInt(numberWithoutSeparators);
   }
 
-  // If no comma-inclusive number is found, try to match any number
-  let wordleWithoutComma = text.match(/wordle\s*#?\s*(\d+)/i);
-  if(wordleWithoutComma && wordleWithoutComma.length >= 2) {
+  // If no formatted number is found, try to match any sequence of digits
+  let wordleWithoutFormatting = text.match(/wordle\s*#?\s*(\d+)/i);
+  if(wordleWithoutFormatting && wordleWithoutFormatting.length >= 2) {
     // Convert found number to integer
-    return parseInt(wordleWithoutComma[1]);
+    return parseInt(wordleWithoutFormatting[1]);
   }
 
   // Return 0 if no matching number is found
