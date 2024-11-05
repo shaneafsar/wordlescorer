6d4053468ef668dba8a0f1643a0084130ff8fe92
Shane Afsar <shane.afsar@gmail.com>
2024-11-05 13:52:05 -0500

    Mongo local ability

--- ts/mongo.ts
+++ ts/mongo.ts
@@ -1,4 +1,11 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
+import dotenv from 'dotenv';
+
+const IS_DEVELOPMENT = process.env['NODE_ENV'] === 'develop';
+
+if (IS_DEVELOPMENT) {
+  dotenv.config();
+};
 
 const uri = `mongodb+srv://${process.env['MONGODB_USER']}:${process.env['MONGODB_PASS']}@cluster0.yztewyz.mongodb.net/?retryWrites=true&w=majority`;
 
